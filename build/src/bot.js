a31d8da560789c2a5ab322e9f8c31c2385945726
Никита Жигилей <nzhigiley@MacBook-Air-Nikita.local>
2024-11-21 17:24:13 +0300

    fixes

--- build/src/bot.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bot = void 0;
-var telegraf_1 = require("telegraf");
-exports.bot = new telegraf_1.Telegraf((_a = process.env.BOT_TOKEN) !== null && _a !== void 0 ? _a : '', {
-    handlerTimeout: Infinity,
-});
