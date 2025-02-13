eb886878ae6eb590aa973416b3bf420a7e4d1eec
Никита Жигилей <nzhigiley@MacBook-Air-Nikita.local>
2025-02-13 07:27:35 +0300

    fixes

--- src/controllers/update-yandex.ts
+++ src/controllers/update-yandex.ts
@@ -55,7 +55,7 @@ export const updateYandex = async (
 				.set('minute', 59)
 				.set('second', 59)
 				.set('milliseconds', 59)
-				.add(0, 'month')
+				.subtract(1, 'month')
 				.format('YYYY-MM-DD'),
 		}
 
