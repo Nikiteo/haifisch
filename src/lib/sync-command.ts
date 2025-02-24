00b1e3f8c20b88d4a46a9136abc2e4ddd217411b
Никита Жигилей <nzhigiley@MacBook-Air-Nikita.local>
2025-02-24 15:02:18 +0300

    new

--- src/lib/sync-command.ts
+++ src/lib/sync-command.ts
@@ -17,8 +17,8 @@ export const syncCommand = (): void => {
 			}
 			await ctx.reply('Начал обновление...')
 			await updateOzon('Ozon', sendMessage)
-			// await updateYandex('Haifisch', sendMessage)
-			// await updateYandex('Top', sendMessage)
+			await updateYandex('Haifisch', sendMessage)
+			await updateYandex('Top', sendMessage)
 		} else {
 			return await ctx.reply('Прости, но ты не можешь использовать меня')
 		}
