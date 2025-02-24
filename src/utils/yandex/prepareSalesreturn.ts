fe4dbd3c314cb7bd8e7901850879bc8704ff26a8
Никита Жигилей <nzhigiley@MacBook-Air-Nikita.local>
2025-02-24 18:18:46 +0300

    fixes

--- src/utils/yandex/prepareSalesreturn.ts
+++ src/utils/yandex/prepareSalesreturn.ts
@@ -1,52 +1,7 @@
-import { states, country } from '../../database'
 import { Logger } from '../../lib'
-
-import {
-	type Demand,
-	type CustomerOrder,
-	type SalesReturn,
-	type Meta,
-} from '../../types/ms-types'
+import { states, country } from '../../database'
+import { Demand, CustomerOrder, SalesReturn } from '../../types/ms-types'
 import { createSalesReturn } from './createSalesreturn'
-
-const filterOrdersByState = (
-	orders: CustomerOrder[],
-	stateMeta: Meta[]
-): CustomerOrder[] => {
-	return orders.filter(order => {
-		const meta = order.state?.meta
-		return meta?.href && stateMeta.some(state => state.href === meta.href)
-	})
-}
-
-const mapDemandToSalesReturn = (
-	demands: Demand[],
-	orders: CustomerOrder[],
-	place?: string
-): Demand[] => {
-	return demands.reduce<Demand[]>((acc, cur) => {
-		const matchingOrder = orders.find(order => order.name === cur.name)
-		if (matchingOrder) {
-			acc.push({
-				...cur,
-				positions: matchingOrder.positions
-					? matchingOrder.positions?.map(pos => ({
-							...pos,
-							country,
-						}))
-					: [],
-			})
-		}
-		return acc
-	}, [])
-}
-
-const createSalesReturnsFromDemands = (
-	demands: Demand[],
-	place?: string
-): SalesReturn[] => {
-	return demands.map(demand => createSalesReturn(demand, place))
-}
 
 export const prepareSalesReturn = (
 	demands: Demand[],
@@ -55,60 +10,193 @@ export const prepareSalesReturn = (
 	place?: string
 ): SalesReturn[] => {
 	try {
-		if (demands.length === 0 || orders.length === 0) {
+		if (demands.length === 0) {
 			return []
 		}
 
-		const isOzon = place === 'OZON'
-		const stateMeta = isOzon
-			? [states.PICKED_REFUND.meta]
-			: [
-					states.CANCELLED.meta,
-					states.CANCELLED_IN_DELIVERY.meta,
-					states.RETURNED.meta,
-					states.PARTIALLY_RETURNED.meta,
-				].filter(meta => meta !== undefined)
-
-		const filteredOrders = filterOrdersByState(orders, stateMeta)
+		if (orders.length === 0) {
+			return []
+		}
 
 		if (salesreturn.length === 0) {
-			const mappedDemands = mapDemandToSalesReturn(
-				demands,
-				filteredOrders,
-				place
-			)
-			return createSalesReturnsFromDemands(mappedDemands, place)
-		} else {
-			const updatedSalesReturn = demands.reduce<SalesReturn[]>(
-				(acc, cur) => {
-					salesreturn.forEach((returns: SalesReturn) => {
-						if (returns.name === cur.name) {
-							const updated = createSalesReturn(cur, place)
-							acc.push({
-								...returns,
-								...updated,
-								organization: returns.organization,
-							})
-						}
-					})
-					return acc
-				},
-				[]
-			)
+			if (place === 'OZON') {
+				const filteredOrders = orders.filter(
+					order =>
+						order.state?.meta === states.RETURNED.meta ||
+						order.state?.meta === states.PARTIALLY_RETURNED.meta
+				)
+				const addPositionsToDemands = demands.reduce<Demand[]>(
+					(acc: Demand[], cur: Demand) => {
+						filteredOrders.forEach(order => {
+							if (order.name === cur.name) {
+								acc.push({
+									...cur,
+									positions: order.positions?.map(pos => {
+										return {
+											...pos,
+											country,
+										}
+									}),
+								})
+							}
+						})
+						return acc
+					},
+					[]
+				)
 
-			const newDemands = demands.filter(demand =>
-				updatedSalesReturn.every(sale => sale.name !== demand.name)
-			)
+				return addPositionsToDemands.reduce(
+					(acc: SalesReturn[], cur: Demand) => {
+						acc.push(createSalesReturn(cur))
+						return acc
+					},
+					[]
+				)
+			} else {
+				const filteredOrders = orders.filter(
+					order =>
+						order.state?.meta === states.CANCELLED.meta ||
+						order.state?.meta ===
+							states.CANCELLED_IN_DELIVERY.meta ||
+						order.state?.meta === states.RETURNED.meta ||
+						order.state?.meta === states.PARTIALLY_RETURNED.meta
+				)
 
-			const newMappedDemands = mapDemandToSalesReturn(
-				newDemands,
-				filteredOrders,
-				place
-			)
-			return [
-				...updatedSalesReturn,
-				...createSalesReturnsFromDemands(newMappedDemands, place),
-			]
+				const addPositionsToDemands = demands.reduce<Demand[]>(
+					(acc: Demand[], cur: Demand) => {
+						filteredOrders.forEach(order => {
+							if (order.name === cur.name) {
+								acc.push({
+									...cur,
+									positions: order.positions?.map(pos => {
+										return {
+											...pos,
+											country,
+										}
+									}),
+								})
+							}
+						})
+						return acc
+					},
+					[]
+				)
+
+				return addPositionsToDemands.reduce(
+					(acc: SalesReturn[], cur: Demand) => {
+						acc.push(createSalesReturn(cur))
+						return acc
+					},
+					[]
+				)
+			}
+		}
+
+		if (salesreturn.length !== 0) {
+			if (place === 'OZON') {
+				const filteredOrders = orders.filter(
+					order =>
+						order.state?.meta === states.RETURNED.meta ||
+						order.state?.meta === states.PARTIALLY_RETURNED.meta
+				)
+				const updatedSalesreturn = demands.reduce(
+					(acc: SalesReturn[], cur: Demand) => {
+						salesreturn.forEach((returns: SalesReturn) => {
+							if (returns.name === cur.name) {
+								const updated = createSalesReturn(cur)
+								acc.push({
+									...returns,
+									...updated,
+									organization: returns.organization,
+								})
+							}
+						})
+						return acc
+					},
+					[]
+				)
+				const findNewDemands = demands.filter(demand =>
+					updatedSalesreturn.every(sale => sale.name !== demand.name)
+				)
+				const addPositionsToDemands = findNewDemands.reduce<Demand[]>(
+					(acc: Demand[], cur: Demand) => {
+						filteredOrders.forEach(order => {
+							if (order.name === cur.name) {
+								acc.push({
+									...cur,
+									positions: order.positions?.map(pos => {
+										return {
+											...pos,
+											country,
+										}
+									}),
+								})
+							}
+						})
+						return acc
+					},
+					[]
+				)
+
+				addPositionsToDemands.forEach(demand => {
+					updatedSalesreturn.push(createSalesReturn(demand))
+				})
+
+				return updatedSalesreturn
+			} else {
+				const filteredOrders = orders.filter(
+					order =>
+						order.state?.meta === states.CANCELLED.meta ||
+						order.state?.meta ===
+							states.CANCELLED_IN_DELIVERY.meta ||
+						order.state?.meta === states.RETURNED.meta ||
+						order.state?.meta === states.PARTIALLY_RETURNED.meta
+				)
+				const updatedSalesreturn = demands.reduce(
+					(acc: SalesReturn[], cur: Demand) => {
+						salesreturn.forEach((returns: SalesReturn) => {
+							if (returns.name === cur.name) {
+								const updated = createSalesReturn(cur)
+								acc.push({
+									...returns,
+									...updated,
+									organization: returns.organization,
+								})
+							}
+						})
+						return acc
+					},
+					[]
+				)
+				const findNewDemands = demands.filter(demand =>
+					updatedSalesreturn.every(sale => sale.name !== demand.name)
+				)
+				const addPositionsToDemands = findNewDemands.reduce<Demand[]>(
+					(acc: Demand[], cur: Demand) => {
+						filteredOrders.forEach(order => {
+							if (order.name === cur.name) {
+								acc.push({
+									...cur,
+									positions: order.positions?.map(pos => {
+										return {
+											...pos,
+											country,
+										}
+									}),
+								})
+							}
+						})
+						return acc
+					},
+					[]
+				)
+
+				addPositionsToDemands.forEach(demand => {
+					updatedSalesreturn.push(createSalesReturn(demand))
+				})
+
+				return updatedSalesreturn
+			}
 		}
 	} catch (err) {
 		Logger.error(err)
