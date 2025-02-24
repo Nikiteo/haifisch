ae54dc1786ac68910f776c390c74c2772ad7d739
Никита Жигилей <nzhigiley@MacBook-Air-Nikita.local>
2025-02-24 16:32:58 +0300

    fixes

--- src/utils/yandex/createDemand.ts
+++ src/utils/yandex/createDemand.ts
@@ -5,52 +5,32 @@ export const createOverhadSum = (
 	attributes: Attribute[],
 	place?: string
 ): number => {
-	if (
-		attributes.filter(attribute => attribute.type === 'double').length === 0
-	) {
+	if (!attributes.some(attribute => attribute.type === 'double')) {
 		return 0
 	}
 
 	if (place === 'OZON') {
-		return parseFloat(
-			(
-				parseFloat(
-					parseFloat(
-						// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions, @typescript-eslint/no-unsafe-argument
-						attributes?.find(
-							attribute =>
-								attribute.id ===
-								'279ba9fa-9d67-11ee-0a80-09f500178da3'
-						)?.value || 0
-					).toFixed(2)
-				) * 100
-			).toFixed(2)
-		)
+		const ozonValue =
+			attributes.find(
+				attribute =>
+					attribute.id === '279ba9fa-9d67-11ee-0a80-09f500178da3'
+			)?.value || 0
+		return parseFloat((ozonValue * 100).toFixed(2))
 	}
 
-	const sumOfComissions = attributes
-		.filter(attribute => attribute.type === 'double')
+	const sumOfCommissions = attributes
 		.filter(
-			attribute => attribute.id !== '2d77bca0-974b-11ee-0a80-146900276f3a'
+			attribute =>
+				attribute.type === 'double' &&
+				attribute.id !== '2d77bca0-974b-11ee-0a80-146900276f3a' &&
+				attribute.id !== '279ba9fa-9d67-11ee-0a80-09f500178da3'
 		)
-		.filter(
-			attribute => attribute.id !== '279ba9fa-9d67-11ee-0a80-09f500178da3'
-		)
-		.map(attribute => attribute.value as number)
-		.reduce(
-			(acc: number, cur: number): number =>
-				parseFloat((acc + cur).toFixed(2)),
-			0
-		)
+		.reduce((acc, attribute) => acc + (attribute.value as number), 0)
 
-	return parseFloat((sumOfComissions * 100).toFixed(2))
+	return parseFloat((sumOfCommissions * 100).toFixed(2))
 }
 
-const createCarrier = (
-	place?: string
-): {
-	meta: Meta
-} => {
+const createCarrier = (place?: string): { meta: Meta } => {
 	switch (place) {
 		case 'OZON':
 			return ozonAgent
@@ -62,30 +42,28 @@ const createCarrier = (
 }
 
 export const createDemand = (order: CustomerOrder, place?: string): Demand => {
-	const {
-		meta,
-		id,
-		accountId,
-		applicable,
-		attributes,
-		owner,
-		organizationAccount,
-		deliveryPlannedMoment,
-		externalCode,
-		syncId,
-		updated,
-		state,
-		sum,
-		agentAccount,
-		created,
-		printed,
-		published,
-		reservedSum,
-		payedSum,
-		shippedSum,
-		invoicedSum,
-		...rest
-	} = order
+	const { meta, attributes, deliveryPlannedMoment, ...rest } = order
+
+	const overheadSum = attributes ? createOverhadSum(attributes, place) : 0
+
+	const additionalAttribute =
+		place === 'OZON' &&
+		attributes?.find(
+			attribute => attribute.id === 'c09d1b3e-90ff-11ef-0a80-0efd00046bc2'
+		)
+	const newAttribute = additionalAttribute
+		? {
+				meta: {
+					href: 'https://api.moysklad.ru/api/remap/1.2/entity/demand/metadata/attributes/807c3874-9100-11ef-0a80-0de10004c634',
+					type: 'attributemetadata',
+					mediaType: 'application/json',
+				},
+				id: '807c3874-9100-11ef-0a80-0de10004c634',
+				name: 'Дата получения возврата',
+				type: 'string',
+				value: additionalAttribute.value,
+			}
+		: undefined
 
 	return {
 		...rest,
@@ -93,34 +71,12 @@ export const createDemand = (order: CustomerOrder, place?: string): Demand => {
 			meta: order.meta,
 		},
 		overhead: {
-			sum:
-				order.attributes !== undefined
-					? createOverhadSum(order.attributes, place)
-					: 0,
+			sum: overheadSum,
 			distribution: 'price',
 		},
-		attributes:
-			place === 'OZON'
-				? [
-						{
-							meta: {
-								href: 'https://api.moysklad.ru/api/remap/1.2/entity/demand/metadata/attributes/807c3874-9100-11ef-0a80-0de10004c634',
-								type: 'attributemetadata',
-								mediaType: 'application/json',
-							},
-							id: '807c3874-9100-11ef-0a80-0de10004c634',
-							name: 'Дата получения возврата',
-							type: 'string',
-							value: order.attributes?.find(
-								attribute =>
-									attribute.id ===
-									'c09d1b3e-90ff-11ef-0a80-0efd00046bc2'
-							)?.value,
-						},
-					]
-				: [],
+		attributes: newAttribute ? [...(attributes || []), newAttribute] : [],
 		consignee,
 		carrier: createCarrier(place),
-		moment: order.deliveryPlannedMoment,
+		moment: deliveryPlannedMoment,
 	}
 }
