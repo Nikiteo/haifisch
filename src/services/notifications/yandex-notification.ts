70343abae8c04e78abdce8860ea98f3d687bb1a2
Никита Жигилей <nzhigiley@MacBook-Air-Nikita.local>
2025-02-26 18:00:02 +0300

    add created order for yandex

--- src/services/notifications/yandex-notification.ts
+++ src/services/notifications/yandex-notification.ts
@@ -26,7 +26,7 @@ yandexRouter.post('/notification', async (req: Request, res: Response) => {
 		time: currentTime,
 	}
 
-	if (campaignId === 23726642 || campaignId === 22176022) {
+	if (campaignId !== 23726642 || campaignId !== 22176022) {
 		const errorResponse: ErrorResponse = {
 			error: {
 				type: 'WRONG_EVENT_FORMAT',
