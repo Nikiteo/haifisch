b60df2a40059f3a43c682bca5c68228abb52a226
Никита Жигилей <nzhigiley@MacBook-Air-Nikita.local>
2025-02-27 10:26:58 +0300

    add created order for yandex

--- src/utils/yandex/create-customer-order.ts
+++ src/utils/yandex/create-customer-order.ts
@@ -99,12 +99,9 @@ export const preparePositions = (
 				.map(item => {
 					const totalSubsidies =
 						item.subsidies?.reduce((a, b) => +a + +b.amount, 0) || 0
-					Logger.info(
-						`item.price = ${(item.price * item.count + totalSubsidies) * 100}`
-					)
 					return {
 						quantity: item.count,
-						price: (item.price * item.count + totalSubsidies) * 100,
+						price: (item.price + totalSubsidies) * 100,
 						discount: 0,
 						vat: 0,
 						assortment: {
