42ed39db7da292efbd7dbbcdb0eb39b027abc80e
Никита Жигилей <nzhigiley@MacBook-Air-Nikita.local>
2025-03-05 15:46:15 +0300

    fixes

--- src/controllers/update-ozon.ts
+++ src/controllers/update-ozon.ts
@@ -169,7 +169,7 @@ export const updateOzon = async (
 			if (returnItem) {
 				return {
 					...order,
-					refundDate: returnItem.logistic?.return_date || null,
+					refundDate: returnItem.logistic?.final_moment || null,
 					status:
 						returnItem.visual?.status?.display_name === 'Уже у вас'
 							? OrderFbsOzonStatus.picked_return
